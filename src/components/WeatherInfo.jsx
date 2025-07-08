63b6d5265a24a0bf94d710279b1060c009830156
Leonardo Maciel <leomaciel279@gmail.com>
2025-07-07 23:36:09 -0300

    Adicionado o componente de informações do clima

---
+++ src/components/WeatherInfo.jsx
@@ -0,0 +1,13 @@
+export default function WeatherInfo({ weather }) {
+    if (!weather) return null;
+
+    return(
+        <div className="text-center mt-4">
+            <h2 className="text-xl font-bold">{weather.name}</h2>
+            <p>Temperatura: {Math.round(weather.main.temp)}°C</p>
+            <p>Condição: {weather.weather[0].description}</p>
+
+            <img src="{`https://openweather.org/img/wn/${weather.weather[0].icon}@2x.png`}" alt="Weather Icon" />
+        </div>
+    )
+}
