29f4e00e045502259ff16a7cbbd24a9bd285200f
Leonardo Maciel <leomaciel279@gmail.com>
2025-07-08 23:51:34 -0300

    Criado o componente de formulário e sua ideia base

--- src/components/WeatherForm.jsx
+++ src/components/WeatherForm.jsx
@@ -1,2 +1,28 @@
 import { useState } from "react";  
 
+export default function WeatherForm({ onSearch }){
+    const [city, setCity] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(!city.trim()) return;
+
+        onSearch(city);
+        setCity('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text"
+                placeholder="Digite a sua cidade: "
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                className="p-2 border rounded"
+              />
+            <button type="submit">
+                Buscar
+            </button>
+        </form>
+    );
+}
