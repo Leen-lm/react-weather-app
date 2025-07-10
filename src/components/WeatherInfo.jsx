1182f42225d92adb4bdfc465fcd187f2d99b3f40
Leonardo Maciel <leomaciel279@gmail.com>
2025-07-09 23:37:36 -0300

    Adicionado as estilizações na div e no h2

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -2,8 +2,8 @@ export default function WeatherInfo({ weather }) {
     if (!weather) return null;
 
     return(
-        <div>
-            <h2>{weather.name}</h2>
+        <div className="text-center ">
+            <h2 className="text-blue-800 text-3xl mb-1.5">{weather.name}</h2>
             <p>Temperatura: {Math.round(weather.main.temp)}°C</p>
             <p>Condição: {weather.weather[0].description}</p>
 
