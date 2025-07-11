591d6081789397a7a6875f15a2fd7187581e12e8
Leonardo Maciel <leomaciel279@gmail.com>
2025-07-11 18:09:09 -0300

    Fiz reparações na chamada da url da api e mudei classes de estilização

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,25 @@ function App() {
 
    const fetchWeather = async (city) => {
     try{
-      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}units=metric`
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
       );
-      const text = await res.text();
-      console.log(text);
       
-
       if(!res.ok){
         throw new Error(`Erro HTTP: ${res.status.text}`);
       }
-
+      
       const data = await res.json();
+      console.log(data);
       setWeather(data);
 
     } catch (error){
       console.error('Erro ao buscar o clima', error);
     }
 
-   }
+   };
 
 return (
-  <div className='max-w-md mx-auto p-4'>
+  <div className='max-w-screen flex-col justify-center mx-auto p-4'>
     <h1 className='text-2xl font-bold mb-4 text-center'>Previsão do Tempo</h1>
     <WeatherInfo weather={weather} />
     <WeatherForm onSearch={fetchWeather}/>
