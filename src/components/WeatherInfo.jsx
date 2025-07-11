3dc1ac1b67830930ebf01439abf059f5a49c7676
Leonardo Maciel <leomaciel279@gmail.com>
2025-07-11 18:10:18 -0300

    Alterações na visibilade da temperatura e icone de clima

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -1,13 +1,20 @@
+import numeral from 'numeral';
+import logoDeClima from '../assets/images/logo-de-clima.png'
+
 export default function WeatherInfo({ weather }) {
     if (!weather) return null;
+
+    const temperatura = parseFloat(weather.main.te)
+
+    const temperaturaFormatada = numeral(temperatura / 10).format('00,0')
 
     return(
         <div className="text-center ">
             <h2 className="text-blue-800 text-3xl mb-1.5">{weather.name}</h2>
-            <p>Temperatura: {Math.round(weather.main.temp)}°C</p>
+            <p>Temperatura: {temperaturaFormatada}°C</p>
             <p>Condição: {weather.weather[0].description}</p>
 
-            <img src="{`https://openweather.org/img/wn/${weather.weather[0].icon}@2x.png`}" alt="Weather Icon" />
+            <img className='w-2xs center' src={logoDeClima} alt="Weather Icon" />
         </div>
     )
 }
