9ee3d7b8c025109c437deb73f1008f2d291bf4e7
Leonardo Maciel <leomaciel279@gmail.com>
2025-07-21 22:51:07 -0300

    Criada a função de traduzir a descriç

---
+++ src/functions/translateText.jsx
@@ -0,0 +1,12 @@
+export default async function TranslateText(text, sourceLanguage, targetLanguage ){
+    try{
+      const res = await fetch (`https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLanguage}&tl=${targetLanguage}&dt=t&q=${encodeURIComponent(text)}`);
+
+      const data = await res.json()
+      const traducao = data[0].map(item => item[0]).join('');
+
+      return await traducao;
+    } catch (error){
+        console.error('Erro na tradução:', error)};
+        return text;
+}
