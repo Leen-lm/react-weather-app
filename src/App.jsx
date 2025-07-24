bcd6ba3205e28b1d2cbc234e4a1feed33ce155fa
Leonardo Maciel <leomaciel279@gmail.com>
2025-07-23 23:22:43 -0300

    Mudando altura e largura da tag main

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,37 @@ import WeatherInfo from './components/WeatherInfo'
 import WeatherForm from './components/WeatherForm'
 
 function App() {
-   const [weather, setWeather] = useState(null);
-   const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+  const [weather, setWeather] = useState(null);
+  const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
-   const fetchWeather = async (city) => {
-    try{
+  const fetchWeather = async (city) => {
+    try {
       const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
       );
-      
-      if(!res.ok){
+
+      if (!res.ok) {
         throw new Error(`Erro HTTP: ${res.status.text}`);
       }
-      
+
       const data = await res.json();
       console.log(data);
       setWeather(data);
 
-    } catch (error){
+    } catch (error) {
       console.error('Erro ao buscar o clima', error);
     }
 
-   };
+  };
 
-return (
-  <div className='max-w-screen flex-col justify-center mx-auto p-4'>
-    <h1 className='text-2xl font-bold mb-4 text-center'>Previsão do Tempo</h1>
-    <WeatherInfo weather={weather} />
-    <WeatherForm onSearch={fetchWeather}/>
-  </div>  
-)
+  return (
+    <main className='w-screen h-screen flex justify-center items-center p-4'>
+      <div>
+        <h1 className='text-2xl font-bold mb-4 text-center'>Previsão do Tempo</h1>
+        <WeatherInfo weather={weather} />
+        <WeatherForm onSearch={fetchWeather} />
+      </div>
+    </main>
+  )
 }
 
 export default App
