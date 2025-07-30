4839b31dcd548b8108be0763a0a8e9a5b369944d
Leonardo Maciel <leomaciel279@gmail.com>
2025-07-29 23:18:58 -0300

    Adicionei outra prop, para verificar se existe dados de clima ou não. Usamos a prop hasWeatherData diretamente na template string das classes, para aplicar condicionalmente os estilos de posição.

--- src/components/Weather/WeatherForm.jsx
+++ src/components/Weather/WeatherForm.jsx
@@ -1,25 +1,28 @@
-import { useState } from "react";  
+import { useState } from "react";
 
-export default function WeatherForm({ onSearch }){
+export default function WeatherForm({ onSearch, hasWeatherData}) {
     const [city, setCity] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(!city.trim()) return;
+        if (!city.trim()) {
+            alert('Por favor, digite uma cidade.')
+            return
+        };
 
         onSearch(city);
-        setCity('');
+        setCity('')
     };
 
     return (
-        <form className="text-center" onSubmit={handleSubmit}>
-            <input 
+        <form className={`text-center ${!hasWeatherData?  'h-[70%]': 'h-auto'} content-center`} onSubmit={handleSubmit}>
+            <input
                 type="text"
                 placeholder="Digite a sua cidade: "
                 value={city}
                 onChange={(e) => setCity(e.target.value)}
                 className="p-2 border rounded"
-              />
+            />
             <button type="submit">
                 Buscar
             </button>
