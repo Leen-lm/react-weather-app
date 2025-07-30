6046eb7b6d9664167f3bdb1776ebad6e1516282d
Leonardo Maciel <leomaciel279@gmail.com>
2025-07-30 18:07:45 -0300

    Alterado o background color do titulo e adicionado border-radius

--- src/components/TitlePage.jsx
+++ src/components/TitlePage.jsx
@@ -1,3 +1,3 @@
 export default function TituloPagina() {
-    return <h1 className='text-4xl font-bold mb-4 text-center border-b-4 p-2.5'>Previsão do Tempo</h1>;
+    return <h1 className='bg-blue-500  text-4xl font-bold mb-4 text-center border-b-4 rounded-t-[18px] p-2.5'>Previsão do Tempo</h1>;
 }
