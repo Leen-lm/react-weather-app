39a5f930b89c265af0b62c644b0cd7707b168a13
Leonardo Maciel <leomaciel279@gmail.com>
2025-07-31 23:50:03 -0300

    Retirei as requisições e deixei no app jsx, apenas oque será renderizado para o usuário final

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,9 @@
-import { useState } from 'react';
-import WeatherInfo from './components/Weather/WeatherInfo'
-import WeatherForm from './components/Weather/WeatherForm'
-import TituloPagina from './components/TitlePage';
+import Weather from './WeatherApp/Weather';
 
 function App() {
-  const [weather, setWeather] = useState(null);
-  const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
-
-  const fetchWeather = async (city) => {
-    try {
-      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-      );
-
-      if (!res.ok) {
-        throw new Error(`Erro HTTP: ${res.status.text}`);
-      }
-
-      const data = await res.json();
-      setWeather(data);
-
-    } catch (error) {
-      console.error('Erro ao buscar o clima', error);
-    }
-
-  };
-
   return (
     <main className='bg-blue-400 w-screen h-screen flex justify-center items-center p-4'>
-      <div className='flex-col border-4 bg-yellow-400 w-170 h-130 rounded-3xl'>
-        <TituloPagina />
-        {weather && <WeatherInfo weather={weather} />}
-        <WeatherForm onSearch={fetchWeather} hasWeatherData={!!weather} />
-      </div>
+        <Weather />
     </main>
   )
 }
