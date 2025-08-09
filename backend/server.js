4238791201c7b9c3c9d242c870f14557d8190a8a
Leonardo Maciel <leomaciel279@gmail.com>
2025-08-08 21:03:23 -0300

    Adicionado origens de requisições, agora tanto o ambiente de produção, quanto no local, pode ter requisições da API no Render.

--- backend/server.js
+++ backend/server.js
@@ -5,22 +5,32 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 
-const corsOptions = {
-    origin: 'https://react-weather-app-seven-brown.vercel.app',
-    optionsSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+const corsOptions =
+    [
+        'https://react-weather-app-seven-brown.vercel.app',
+        'http://localhost:5173'
+    ];
+
+app.use(cors({
+    origin: function (origin, callback){
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/weather", async (req, res) => {
-    const {city} = req.query;
+    const { city } = req.query;
 
-    if(!city){
+    if (!city) {
         return res.status(400).json({ error: "Cidade não informada!" });
     }
 
-    try{
+    try {
         const apiKey = process.env.WEATHER_API_KEY;
         const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
@@ -35,5 +45,5 @@ app.get("/weather", async (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
-    
+
 })
