9e5735d9de7003e19c82afbe106ca46b587527e6
Leonardo Maciel <leomaciel279@gmail.com>
2025-08-19 21:22:58 -0300

    Adicionado o erro após o form. Com a condição de que se o weather for false/inválido, o erro será true e renderizado.

--- frontend/src/WeatherApp/Weather.jsx
+++ frontend/src/WeatherApp/Weather.jsx
@@ -6,13 +6,18 @@ import TituloPagina from "../components/TitlePage";
 
 export default function Weather() {
     const [city, setCity] = useState('');
-    const weather = useWeather(city);
+    const { weather, error } = useWeather(city);
 
     return (
         <div className='flex-col border-4 bg-yellow-400 w-170 h-130 rounded-3xl'>
             <TituloPagina />
+
             {weather && <WeatherInfo weather={weather} />}
+
             <WeatherForm onSearch={setCity} hasWeatherData={!!weather} />
+            {error && <p className="text-center text-[22px] text-red-600 font-bold mt-[-100px]">{error}</p>}
+
+            
         </div>
     )
 }
