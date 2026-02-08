6cf7206c750ba87cb819eea9e833b39f2722ec34
Leonardo Maciel <leomaciel279@gmail.com>
2026-02-08 17:51:54 -0300

    feature: Adicionei na interface de clima, a mensagem de 'Carregando clima do tempo', até a API retornar os dados.

--- frontend/src/WeatherApp/Weather.jsx
+++ frontend/src/WeatherApp/Weather.jsx
@@ -6,12 +6,12 @@ import TituloPagina from "../components/TitlePage";
 
 export default function Weather() {
     const [city, setCity] = useState('');
-    const { weather, error } = useWeather(city);
+    const { weather, error, loading } = useWeather(city);
 
     return (
         <div className='flex-col border-4 bg-yellow-400 w-170 h-130 rounded-3xl'>
             <TituloPagina />
-
+            {loading && <p className= "text-center text-[22px] text-blue-600 font-bold">Carregando clima do tempo ...</p>}
             {weather && <WeatherInfo weather={weather} />}
 
             <WeatherForm onSearch={setCity} hasWeatherData={!!weather} />
